2afc209c69de42f9837251a107475b85cba047d8
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2025-09-30 16:20:51 -0300

    feat(docker): configuracion de docker para back y db

--- src/mailer/mailer.controller.ts
+++ src/mailer/mailer.controller.ts
@@ -1,34 +1,34 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { MailerService } from './mailer.service';
-import { NewsLetterFormDto } from './newsLetterForm.dto';
+// import { Body, Controller, Post } from '@nestjs/common';
+// import { MailerService } from './mailer.service';
+// import { NewsLetterFormDto } from './newsLetterForm.dto';
 
-@Controller('mailer')
-export class MailerController {
-  constructor(private readonly appService: MailerService) {}
+// @Controller('mailer')
+// export class MailerController {
+//   constructor(private readonly appService: MailerService) {}
 
-  @Post('newsletter')
-  async sendNewsletterMailController(
-    @Body() newsLetterForm: NewsLetterFormDto
-  ) {
-    if (
-      !newsLetterForm.title ||
-      !newsLetterForm.subtitle ||
-      !newsLetterForm.description
-    )
-      return 'Titulo, Subtitulo y Descripción son requeridos.';
+//   @Post('newsletter')
+//   async sendNewsletterMailController(
+//     @Body() newsLetterForm: NewsLetterFormDto
+//   ) {
+//     if (
+//       !newsLetterForm.title ||
+//       !newsLetterForm.subtitle ||
+//       !newsLetterForm.description
+//     )
+//       return 'Titulo, Subtitulo y Descripción son requeridos.';
 
-    if (newsLetterForm.title.length < 5 || newsLetterForm.title.length > 20)
-      return 'Titulo, debe tener entre 5 y 20 caracteres.';
+//     if (newsLetterForm.title.length < 5 || newsLetterForm.title.length > 20)
+//       return 'Titulo, debe tener entre 5 y 20 caracteres.';
 
-    if (
-      newsLetterForm.subtitle.length < 5 ||
-      newsLetterForm.subtitle.length > 20
-    )
-      return 'Subtitulo, debe tener entre 5 y 20 caracteres.';
+//     if (
+//       newsLetterForm.subtitle.length < 5 ||
+//       newsLetterForm.subtitle.length > 20
+//     )
+//       return 'Subtitulo, debe tener entre 5 y 20 caracteres.';
 
-    if (newsLetterForm.description.length < 5)
-      return 'Descripción, debe tener mas de 5 caracteres.';
+//     if (newsLetterForm.description.length < 5)
+//       return 'Descripción, debe tener mas de 5 caracteres.';
 
-    return await this.appService.sendNewsletterMail(newsLetterForm);
-  }
-}
+//     return await this.appService.sendNewsletterMail(newsLetterForm);
+//   }
+// }
